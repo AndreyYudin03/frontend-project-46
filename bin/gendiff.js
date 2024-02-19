392cadb8c7e3e0f4c7866b97366dbc49d04e7833
andrey yudin <onetapandrey@gmail.com>
2024-02-19 13:42:04 +0300

    create project

---
+++ bin/gendiff.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const { Command } = require('commander');
+
+const program = new Command();
+
+program
+  .version('1.0.0')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .arguments('<filepath1> <filepath12>')
+  .parse(process.argv);
