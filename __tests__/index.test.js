0df9e0719b74013e0c7fc315e5eb7f7fbf3f1ee3
andrey yudin <onetapandrey@gmail.com>
2024-02-22 14:39:47 +0300

    step 5 test2

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,7 +2,7 @@ import getDiffFiles from '../src/index.js';
 
 test('flat json files', () => {
   const result = `{
-  -follow: false
+  - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
   - timeout: 50
@@ -10,6 +10,6 @@ test('flat json files', () => {
   + verbose: true
 }`;
   expect(
-    getDiffFiles('__fixtures__/file1.json', '__fixtures__/file2.json')
+    getDiffFiles('__fixtures__/file1.json', '__fixtures__/file2.json'),
   ).toBe(result);
 });
