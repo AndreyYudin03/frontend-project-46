274a97ce4dc1b52156f6998e5794011a85c7e0a6
andrey yudin <onetapandrey@gmail.com>
2024-04-15 23:14:58 +0300

    refactoring

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,31 +1,38 @@
 import fs from 'fs';
 import getDiffFiles from '../src/index.js';
 
-const readFixtureFile = (filename) => fs.readFileSync(`__fixtures__/results/${filename}`, 'utf8');
+const readFixtureFile = (filename) =>
+  fs.readFileSync(`__fixtures__/results/${filename}`, 'utf8');
 
 const FilesPath = (filePath1, filePath2) => [
   `__fixtures__/files/${filePath1}`,
   `__fixtures__/files/${filePath2}`,
 ];
 
+const createExpectedResult = (stylishFile, plainFile, jsonFile) => ({
+  stylish: readFixtureFile(stylishFile),
+  plain: readFixtureFile(plainFile),
+  json: readFixtureFile(jsonFile),
+});
+
 const testCases = [
   {
     name: 'deep json files 1',
     inputFiles: FilesPath('file1.json', 'file2.json'),
-    expected: {
-      stylish: readFixtureFile('stylish_result_1.txt'),
-      plain: readFixtureFile('plain_result_1.txt'),
-      json: readFixtureFile('json_result_1.txt'),
-    },
+    expected: createExpectedResult(
+      'stylish_result_1.txt',
+      'plain_result_1.txt',
+      'json_result_1.txt'
+    ),
   },
   {
     name: 'deep yaml files 1',
     inputFiles: FilesPath('file3.yaml', 'file4.yaml'),
-    expected: {
-      stylish: readFixtureFile('stylish_result_2.txt'),
-      plain: readFixtureFile('plain_result_2.txt'),
-      json: readFixtureFile('json_result_2.txt'),
-    },
+    expected: createExpectedResult(
+      'stylish_result_2.txt',
+      'plain_result_2.txt',
+      'json_result_2.txt'
+    ),
   },
 ];
 
